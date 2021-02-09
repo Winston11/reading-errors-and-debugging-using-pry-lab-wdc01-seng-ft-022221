59fbafb8d6e835fc48a6e3ac1aa62038beffb968
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-09 23:55:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,8 +9,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(state_log)
-  puts state_log(date)
-  date = generate_star_date
-  binding.pry
+def engage
+  puts state_log
+  puts "Captain's Log, star date #{star_date}."
 end
